b526660977900cb1f6972bf52ec2a90d7ba98970
eternityzqf <1583510917@qq.com>
2018-07-17 09:50:54 +0800

    修改Activity

--- app/src/main/java/com/zqf/ndkaudiovideodemo/MainActivity.java
+++ app/src/main/java/com/zqf/ndkaudiovideodemo/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import java.io.File;
 import java.io.IOException;
